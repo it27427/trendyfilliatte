789b6cb2dafd3a2e38d7fac8c506d60a63550d95
it27427 <istiak.tushar.qtec@gmail.com>
2025-04-16 22:55:18 +0600

    category-admin

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . models import Category
 
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+  list_display = ('id', 'category_name', 'created_at', 'updated_at')
+
+admin.site.register(Category)
