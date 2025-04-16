2cac123da2ebf34f00f8f8165b5ccf73ed9d2379
it27427 <istiak.tushar.qtec@gmail.com>
2025-04-17 02:13:14 +0600

    category-menu created

--- blogs/admin.py
+++ blogs/admin.py
@@ -8,6 +8,8 @@ class CategoryAdmin(admin.ModelAdmin):
 class BlogAdmin(admin.ModelAdmin):
   list_display = ('id', 'title', 'slug', 'category', 'author', 'blog_image', 'short_description', 'blogs_body', 'status', 'is_featured', 'created_at', 'updated_at')
   pre_populated_fields = {'slug': ('title',)}
+  search_fields = ('id', 'title', 'category__category_name', 'status')
+  list_editable = ('is_featured',)
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Blogs, BlogAdmin)
