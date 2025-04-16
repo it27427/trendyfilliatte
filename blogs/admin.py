6bc59fb1a305a4ceef1c4c457a4ca7f51b56373d
it27427 <istiak.tushar.qtec@gmail.com>
2025-04-17 00:59:01 +0600

    BlogAdmin Registered in Admin

--- blogs/admin.py
+++ blogs/admin.py
@@ -4,6 +4,10 @@ from .models import Category,Blogs
 
 class CategoryAdmin(admin.ModelAdmin):
   list_display = ('id', 'category_name', 'created_at', 'updated_at')
+  
+class BlogAdmin(admin.ModelAdmin):
+  list_display = ('id', 'title', 'slug', 'category', 'author', 'blog_image', 'short_description', 'blogs_body', 'status', 'is_featured', 'created_at', 'updated_at')
+  pre_populated_fields = {'slug': ('title',)}
 
 admin.site.register(Category, CategoryAdmin)
-admin.site.register(Blogs)
+admin.site.register(Blogs, BlogAdmin)
