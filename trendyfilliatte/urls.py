02ee72c017f61a51192ba15c99cf8e1c9347a0e5
it27427 <istiak.tushar.qtec@gmail.com>
2025-04-17 05:35:01 +0600

    urlpatters updated

--- trendyfilliatte/urls.py
+++ trendyfilliatte/urls.py
@@ -8,6 +8,15 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
 handler404 = 'trendyfilliatte.views.error_404_view'
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
