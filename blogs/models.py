f843848e5321b55a779eb328d3053036787ad094
it27427 <istiak.tushar.qtec@gmail.com>
2025-04-17 00:52:16 +0600

    Blogs renamed in admin

--- blogs/models.py
+++ blogs/models.py
@@ -28,3 +28,6 @@ class Blogs(models.Model):
   is_featured = models.BooleanField(default=False)
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
+  
+  class Meta:
+    verbose_name_plural = 'Blogs'
