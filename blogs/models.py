eeb23087095980e04432d594e535f8497759e2ac
it27427 <istiak.tushar.qtec@gmail.com>
2025-04-17 00:49:24 +0600

    blogs model created

--- blogs/models.py
+++ blogs/models.py
@@ -1,5 +1,6 @@
 # models.py
 from django.db import models
+from django.contrib.auth.models import User
 
 class Category(models.Model):
   category_name = models.CharField(max_length=50, unique=True)
@@ -8,3 +9,22 @@ class Category(models.Model):
 
   class Meta:
     verbose_name_plural = 'Categories'
+    
+
+STATUS_CHOICE = (
+    ('draft', 'Draft'),
+    ('public', 'Published'),
+)    
+
+class Blogs(models.Model):
+  title = models.CharField(max_length=100, unique=True)
+  slug = models.SlugField(unique=True, blank=True)
+  category = models.ForeignKey(Category, on_delete=models.CASCADE)
+  author = models.ForeignKey(User, on_delete=models.CASCADE)
+  blog_image = models.ImageField(upload_to='uploads/%y/%m/%d')
+  short_description = models.TextField(max_length=2000)
+  blogs_body = models.TextField(max_length=5000)
+  status = models.IntegerField(choices = STATUS_CHOICE, default='draft')
+  is_featured = models.BooleanField(default=False)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
