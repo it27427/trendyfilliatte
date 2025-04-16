212e805598d82780e45a546a4d0544e5c65a107a
it27427 <istiak.tushar.qtec@gmail.com>
2025-04-17 00:51:03 +0600

    blogs model registered

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,8 +1,9 @@
 # admin.py
 from django.contrib import admin
-from .models import Category
+from .models import Category,Blogs
 
 class CategoryAdmin(admin.ModelAdmin):
   list_display = ('id', 'category_name', 'created_at', 'updated_at')
 
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Blogs)
