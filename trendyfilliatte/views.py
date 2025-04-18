7552cf7e7aa8a9015c30d7d5620c42003c1c1df8
it27427 <istiak.tushar.qtec@gmail.com>
2025-04-18 21:43:57 +0600

    featured_blog_post data shows in template

--- trendyfilliatte/views.py
+++ trendyfilliatte/views.py
@@ -5,8 +5,6 @@ from blogs.models import Category, Blogs
 def home(request):
     categories = Category.objects.all()
     featured_post = Blogs.objects.filter(is_featured=True)
-    # Get the latest blogs from each category
-    print(featured_post)
     context = {
         'categories': categories,
         'featured_post': featured_post,
