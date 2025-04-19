288539adeed0fedacd37a4b2a5ec31e888c18d1f
it27427 <istiak.tushar.qtec@gmail.com>
2025-04-19 23:21:07 +0600

    post

--- trendyfilliatte/views.py
+++ trendyfilliatte/views.py
@@ -5,6 +5,7 @@ from blogs.models import Category, Blogs
 def home(request):
     categories = Category.objects.all()
     featured_post = Blogs.objects.filter(is_featured=True)
+    post = Blogs.objects.filter(is_featured=False, status='published')
     context = {
         'categories': categories,
         'featured_post': featured_post,
