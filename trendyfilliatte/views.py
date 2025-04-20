0f26514b479c2a9be6706a7bc471dc5b82ef3aab
it27427 <istiak.tushar.qtec@gmail.com>
2025-04-20 21:41:00 +0600

    posts object updated

--- trendyfilliatte/views.py
+++ trendyfilliatte/views.py
@@ -5,12 +5,12 @@ from blogs.models import Category, Blogs
 def home(request):
     categories = Category.objects.all()
     featured_post = Blogs.objects.filter(is_featured=True, status='published')
-    post = Blogs.objects.filter(is_featured=False, status='published')
+    posts = Blogs.objects.filter(is_featured=False, status='published')
 
     context = {
         'categories': categories,
         'featured_post': featured_post,
-        'post': post,
+        'posts': posts,
     }
     return render(request, 'home.html', context)
 
